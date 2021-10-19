c8e9b73d36dca06b7e1a36724a3c98c25d06b457
RickLiu <mushiking02134tc@gmail.com>
2021-10-19 22:46:07 +0800

    # import bootstrap 5 # deal with nav bar

--- FormSystem/FormSystem/App_Start/BundleConfig.cs
+++ FormSystem/FormSystem/App_Start/BundleConfig.cs
@@ -5,7 +5,6 @@ namespace FormSystem
 {
     public class BundleConfig
     {
-        // 如需統合的詳細資訊，請瀏覽 https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
@@ -14,17 +13,11 @@ namespace FormSystem
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
-            // 使用開發版本的 Modernizr 進行開發並學習。然後，當您
-            // 準備好可進行生產時，請使用 https://modernizr.com 的建置工具，只挑選您需要的測試。
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/bootstrap-5.1.3/js/bootstrap.bundles.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/bootstrap-5.1.3/css/bootstrap.min.css"));
         }
     }
 }
--- FormSystem/FormSystem/Controllers/HomeController.cs
+++ FormSystem/FormSystem/Controllers/HomeController.cs
@@ -8,6 +8,7 @@ namespace FormSystem.Controllers
 {
     public class HomeController : Controller
     {
+
         public ActionResult Index()
         {
             return View();
